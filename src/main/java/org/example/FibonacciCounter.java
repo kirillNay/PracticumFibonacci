d17f3cd6a4de9290e18401d731e40e2d027a39ff
k.naydyuk <k.naydyuk@okko.tv>
2023-10-18 20:55:27 +0300

    Добавили StringBuilder

--- src/main/java/org/example/FibonacciCounter.java
+++ src/main/java/org/example/FibonacciCounter.java
@@ -3,19 +3,19 @@ package org.example;
 public class FibonacciCounter {
 
     String getSequence(int length) {
-        String result = "";
+        StringBuilder result = new StringBuilder();
         int firstValue = 1;
         int secondValue = 0;
         for (int i = 1; i <= length; i++) {
             int sum = firstValue + secondValue;
 
-            result = result + String.format("%d. %d + %d = %d\n", i, secondValue, firstValue, sum);
+            result.append(String.format("%d. %d + %d = %d\n", i, secondValue, firstValue, sum));
 
             secondValue = firstValue;
             firstValue = sum;
         }
 
-        return result;
+        return result.toString();
     }
 
 }
